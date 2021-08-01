7185116bf3a8c267e19d14265ec86f4b2318d14d
susang <susangmree@gmail.com>
2021-08-01 14:32:52 +0545

    logout endpoint added

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import os
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'StoreThisAsEnvVar'
 api = Api(app)
 
@@ -26,6 +29,10 @@ def add_claims_to_jwt(identity):
     if identity == 1: # insdtead of hard coding 1 we should be reading from a config file or database 
         return {'is_admin': True}
     return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
 
 @jwt.expired_token_loader
 def expired_token_callback():
@@ -70,6 +77,7 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 if __name__ == '__main__':
     from db import db 
--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,17 @@
+from blacklist import BLACKLIST
+from flask_jwt_extended.view_decorators import jwt_required
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
     jwt_refresh_token_required,
-    get_jwt_identity
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
 )
 from models.user import UserModel
+from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument(
@@ -60,6 +65,13 @@ class UserLogin(Resource):
                 'refresh_token': refresh_token
             }, 200
         return {'message': 'Invalid credentials'}, 401
+    
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti'] # jti is "JWT ID", a unique identifier for a JWT
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully logged out'}, 200
     
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
